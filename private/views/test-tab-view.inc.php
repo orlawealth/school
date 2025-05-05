5ce6af5b803d55338df6186a64bfb73a1a503584
orlawealth <luv4junior@yahoo.com>
2025-05-05 10:02:30 +0100

    Adding dropdown to test question tab

--- private/views/test-tab-view.inc.php
+++ private/views/test-tab-view.inc.php
@@ -9,3 +9,5 @@
 <a href="<?=ROOT?>/single_test/<?=$row->class_id?>">
     <button class="btn btn-sm btn-primary"><i class="fa fa-plus"></i>Add Objective Question</button>
 </a>
+
+<!-- Add dropdown button -->
